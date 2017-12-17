4c2c19ba0539b359c683eee85665a91d70cb18f7
kuch <liugq5713@gmail.com>
2017-12-17 22:02:26 +0800

    万事开头难

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  githubClientID: "128469dddb23cfd282a8",
+  githubClientSecret: "9d1e6625e406088d50cbdd685af48c3698bd7bb9"
+};
--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const express = require("express");
 const app = express();
 
 app.listen(5000, () => {
-  console.log("hi");
+  console.log("hidd");
 });
